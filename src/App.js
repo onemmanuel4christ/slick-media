abf27ccf8a42b000d2d998a5a1d1dcfb6a94d0f9
onemmanuel4christ <onemmanuel4christ@gmail.com>
2022-12-09 11:28:53 +0100

    slide

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Movies from "./components/Movies";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import Slider from "react-slick";
 
 const Movies = () => {
+     var settings = {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+              breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
   return (
-    <div className='px-[77px]'>
-      Movies
+    <div className='md:px-[77px] px-[30px] w-full mt-[63px]'>
+      <div className='w-full'>
+        <label className='w-full text-[24px] text-[#000]'>Search</label>
+        <input type="text" className='w-full outline-none border border-[#000000] h-[54px] px-5' />
+      </div>
+      <div className='pt-[48px]'>
+        <h1 className='text-[24px] font-normal text-[#000000] mb-[18px]'>Movie Category Name</h1>
+        <Slider {...settings}>
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>
+                Movie Name
+            </div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+            <div className='w-[300px] h-[300px] rounded-md bg-[#000000] flex justify-center items-center text-[#ffffff] p-[10px]'>Movie Name</div>     
+        </Slider>
+      </div>
     </div>
   )
 }
